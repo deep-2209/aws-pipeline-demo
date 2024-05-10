16371a07bc4f082dd9b6756e3cbc9aa6c81da3a8
Patel Deep <deeppatel2049@gmail.com>
2024-05-10 20:08:07 +0530

    Commit 1

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-containerid=`docker ps | awk -F " " '{print $1}'`
-docker rm -f $containerid
+
+# Stop and remove all running Docker containers
+docker stop $(docker ps -q)
+
+# Remove all stopped Docker containers
+docker rm $(docker ps -a -q)
