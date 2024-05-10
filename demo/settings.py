ced6f0a0f2af25cf41341bb65a41a8a61eb73dd1
Patel Deep <deeppatel2049@gmail.com>
2024-05-10 19:26:27 +0530

    Commit

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@fdkb(np3pcd^@uh8oj2n_hel)m3f@!s!rg#j6ywfcd45h3r62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
